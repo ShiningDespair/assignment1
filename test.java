80460150d689b766c3ecc685c5deb2c84cee2b7e
Mehmet Sezen <mehmet.sezen19072@gmail.com>
2023-11-26 15:12:29 +0300

    added question 1

---
+++ test.java
@@ -0,0 +1,9 @@
+class test {
+    public static void main(String[] args) {
+        boolean iremSalak = true;
+        double ireminBoyu = 1.48;
+        String ireminSoyadi = "Ay";
+
+        System.out.println(ireminSoyadi);
+    }
+}
